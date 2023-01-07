7a2127bd85d707a8c7ca0a0ae3268ab2694a07cb
Braifz <braian94lp@gmail.com>
2023-01-07 01:16:42 -0300

    create footer component

---
+++ components/Footer.js
@@ -0,0 +1,51 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import github from "../assets/github.svg";
+import linkedin from "../assets/linkedin.svg";
+import twitter from "../assets/twitter.svg";
+
+const Footer = () => {
+  return (
+    <div className="border-solid border-4 border-lime-500 flex flex-row font-bold justify-between h-24 mt-8">
+      <Link
+        className="border-solid border-r-4 border-lime-500 w-32 flex items-center justify-center hover:bg-lime-500"
+        target="_blank"
+        href="https://braifz.hashnode.dev/"
+      >
+        <Image
+          className="invert hover:invert-0"
+          src={linkedin}
+          alt="github icon"
+          width={50}
+        />
+      </Link>
+      <Link
+        className="border-solid border-r-4 border-l-4 border-lime-500 w-32 flex items-center justify-center hover:bg-lime-500 "
+        target="_blank"
+        href="https://braifz.hashnode.dev/"
+      >
+        <Image
+          className="invert hover:invert-0"
+          src={github}
+          alt="github icon"
+          width={50}
+        />
+      </Link>
+      <Link
+        className="border-solid border-l-4 border-lime-500 w-32 flex items-center justify-center hover:bg-lime-500"
+        target="_blank"
+        href="https://braifz.hashnode.dev/"
+      >
+        <Image
+          className="invert hover:invert-0"
+          src={twitter}
+          alt="github icon"
+          width={50}
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default Footer;
